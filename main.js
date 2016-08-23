7f4dd37d979a7a41a809141eb98e41db1780ce7f
Kevin Grad <kgrad@rethinksolutions.com>
2016-08-23 15:18:49 -0400

    created custom const components and bound to functions

--- main.js
+++ main.js
@@ -2,23 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 
+// var App = React.createClass({
+//   getInitialState: function () {
+//     return {text: ''};
+//   },
+//   componentDidMount: function(){
+//     fetch('http://localhost:8080/itamlink/status')
+//     .then(data => data.text())
+//     .then(text => this.setState({text: text}))
+//     .catch(err => console.log(err))
+//   },
+//   render: function() {
+//
+//     return <div>Response - {this.state.text}</div>
+//   }
+// })
 
-let stateTree = createStore
 
-var App = React.createClass({
-  getInitialState: function () {
-    return {text: ''};
-  },
-  componentDidMount: function(){
-    fetch('http://localhost:8080/itamlink/status')
-    .then(data => data.text())
-    .then(text => this.setState({text: text}))
-    .catch(err => console.log(err))
-  },
-  render: function() {
-
-    return <div>Response - {this.state.text}</div>
+// reducer function
+const counter = (state = 0, action) => {
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } else if (action.type === 'DECREMENT') {
+    return state - 1;
+  } else {
+    return state;
   }
-})
+}
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+
+);
+
+const Input = ({onType}) => (
+  <input onKeyUp={onType} />
+);
+
+// create a store with a reducer method (counter)
+const store = createStore(counter);
+
+
+const render = () => {
+  ReactDOM.render(<Counter value={store.getState()}
+  onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+  onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+  />, document.getElementById('app'))
+
+  ReactDOM.render(<Input onType={() => store.dispatch({type: 'INCREMENT'})}/>,
+    document.getElementById('login'))
+}
+store.subscribe(render);
+render();
