e99394b515f6ca9311e6d14011e6b6f71ac5df4e
Kevin Grad <kgrad@rethinksolutions.com>
2016-08-24 11:16:38 -0400

    added the toggle-todo reducer

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {
+    createStore
+} from 'redux';
 
 // var App = React.createClass({
 //   getInitialState: function () {
@@ -18,58 +20,87 @@ import {createStore} from 'redux';
 //   }
 // })
 
-
 // reducer function
 const todos = (state = [], action) => {
-  switch(action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if (todo.id !== action.id){
-          return todo;
-        }
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state, {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ];
+        case 'TOGGLE_TODO':
+            return state.map(todo => {
+                if (todo.id !== action.id) {
+                    return todo;
+                }
 
-        return {
-          ...todo,
-          completed: !todo.completed
-        };
-      });
+                return {
+                    ...todo,
+                    completed: !todo.completed
+                };
+            });
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 };
 
 const testAddTodo = () => {
-  const stateBefore = [];
-  const action = {
-    type: 'ADD_TODO',
-    id: 0,
-    text: 'Hello'
-  };
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Hello',
-      completed: false
-    }
-  ];
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Hello'
+    };
+    const stateAfter = [{
+        id: 0,
+        text: 'Hello',
+        completed: false
+    }];
 
-  deepFreeze(stateBefore);
-  deepFreeze(action);
+    deepFreeze(stateBefore);
+    deepFreeze(action);
 
-  expect(
-    todos(stateBefore, action)
-  ).toEqual(stateAfter);
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+    const stateBefore = [{
+        id: 0,
+        text: 'Hello',
+        completed: false
+    }, {
+        id: 1,
+        text: 'GoodBye',
+        completed: false
+    }];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    }
+    const stateAfter = [{
+        id: 0,
+        text: 'Hello',
+        completed: false
+    }, {
+        id: 1,
+        text: 'GoodBye',
+        completed: true
+    }];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(todos(stateBefore, action)).toEqual(stateAfter);
+
+}
+
 testAddTodo();
+testToggleTodo();
+
 console.log('All tests passed!')
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
 module.exports = {
-  entry: './main.js',
-  output: {
-    path: './',
-    filename: 'index.js'
-  },
-  devServer: {
-    inline: true,
-    port: 3000
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react', 'stage-2']
-        }
-      }
-    ]
-  }
+    entry: './main.js',
+    output: {
+        path: './',
+        filename: 'index.js'
+    },
+    devServer: {
+        inline: true,
+        port: 3000
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react', 'stage-2']
+            }
+        }]
+    }
 }
